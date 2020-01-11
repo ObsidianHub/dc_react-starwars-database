a5f5c503fea8ecdec458d94b01d4a2263c7c8c5d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-11 11:55:35 +0200

    add transform functions for our API

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,15 +1,16 @@
 export default class SwapiService {
     _apiBase = 'https://swapi.co/api';
+    _imageBase = "https://starwars-visualguide.com/assets/img";
 
-    async getResource(url) {
+    getResource = async url => {
         const res = await fetch(`${this._apiBase}${url}`);
 
         if (!res.ok) {
-            throw new Error(`Couldn't fetch ${url}, received ${res.status}`)
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
         }
 
         return await res.json();
-    }
+    };
 
     async getAllPeople() {
         const res = await this.getResource('/people/');
@@ -37,4 +38,42 @@ export default class SwapiService {
     getStarship(id) {
         return this.getResource(`/starships/${id}`);
     }
+
+    _transformPlanet = planet => {
+        return {
+          id: this._extractId(planet),
+          name: planet.name,
+          diameter: planet.diameter,
+          gravity: planet.gravity,
+          orbitalPeriod: planet.orbital_period,
+          population: planet.population,
+          terrain: planet.terrain,
+          climate: planet.climate,
+          rotationPeriod: planet.rotation_period
+        };
+      };
+    
+      _transformStarship = starship => {
+        return {
+          id: this._extractId(starship),
+          name: starship.name,
+          model: starship.model,
+          manufacturer: starship.manufacturer,
+          costInCredits: starship.cost_in_credits,
+          length: starship.length,
+          crew: starship.crew,
+          passengers: starship.passengers,
+          cargoCapacity: starship.cargo_capacity
+        };
+      };
+    
+      _transformPerson = person => {
+        return {
+          id: this._extractId(person),
+          name: person.name,
+          gender: person.gender,
+          birthYear: person.birth_year,
+          eyeColor: person.eye_color
+        };
+      };
 }
