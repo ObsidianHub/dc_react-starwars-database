c7d66b1a0ad8588a56ee7f422653b4ee6998e38c
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-12 23:34:04 +0200

    add render method in item-details

---
+++ src/components/item-details/item-details.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+import "./item-details.css";
+import ErrorButton from "../error-button";
+
+const Record = ({ item, field, label }) => {
+  return (
+    <li className="list-group-item">
+      <span className="term">{label}</span>
+      <span>{item[field]}</span>
+    </li>
+  );
+};
+
+export { Record };
+
+export default class ItemDetails extends Component {
+  state = {
+    item: null,
+    image: null
+  };
+
+  render() {
+    const { item, image } = this.state;
+
+    if (!item) {
+      return <span>Select a item from a list</span>;
+    }
+
+    const { name } = item;
+
+    return (
+      <div className="person-details card">
+        <div className="image-container">
+          <img className="person-image" src={image} alt="Person" />
+        </div>
+
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            {React.Children.map(this.props.children, child => {
+              return React.cloneElement(child, { item });
+            })}
+          </ul>
+          <ErrorButton />
+        </div>
+      </div>
+    );
+  }
+}
