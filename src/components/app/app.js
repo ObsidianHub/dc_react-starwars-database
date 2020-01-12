c8e88de2bf6bbd009723369feedecd8642121b0d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-12 23:29:21 +0200

    add component error-button and little edit app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,8 +4,18 @@ import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ErrorBoundry from "../error-boundry";
 import SwapiService from "../../services/swapi-service";
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  LoginPage,
+  SecretPage
+} from "../pages";
+
+import "./app.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { StarshipDetails } from "../sw-components";
 
 export default class App extends Component {
   state = {
