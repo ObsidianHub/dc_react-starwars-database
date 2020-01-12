fb7e7759292b56675abe8ac529dcf621afa3c4ef
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-12 23:44:51 +0200

    add starship-details render

---
+++ src/components/sw-components/starship-details.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ItemDetails, { Record } from "../item-details/item-details";
+import ErrorBoundry from "../error-boundry";
+import withSwapiService from "../hoc-helpers/with-swapi-service";
+
+const StarshipDetails = props => {
+  return (
+    <ErrorBoundry>
+      <ItemDetails {...props}>
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" />
+      </ItemDetails>
+    </ErrorBoundry>
+  );
+};
+
+const mapMethodsToProps = swapiService => {
+  return {
+    getData: swapiService.getStarship,
+    getImageUrl: swapiService.getStarshipImage
+  };
+};
+
+export default withSwapiService(mapMethodsToProps)(StarshipDetails);
