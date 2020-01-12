82ff4816938f84258e162a13774e2c7fb036b0a2
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-12 23:34:24 +0200

    add logic to item-details component

--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -20,6 +20,34 @@ export default class ItemDetails extends Component {
     image: null
   };
 
+  componentDidMount() {
+    this.updateItem();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.itemId !== prevProps.itemId ||
+      this.props.getData !== prevProps.getData ||
+      this.props.getImageUrl !== prevProps.getImageUrl
+    ) {
+      this.updateItem();
+    }
+  }
+
+  updateItem() {
+    const { itemId, getData, getImageUrl } = this.props;
+    if (!itemId) {
+      return;
+    }
+
+    getData(itemId).then(item => {
+      this.setState({
+        item,
+        image: getImageUrl(item)
+      });
+    });
+  }
+
   render() {
     const { item, image } = this.state;
 
