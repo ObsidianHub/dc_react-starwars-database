f98ed8fe02b0731beb90c7e1a6f6fbdf93ad3c42
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-13 02:06:04 +0200

    add prop-types wrapper methods

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -15,6 +15,24 @@ const renderModelAndName = ({ model, name }) => (
   </span>
 );
 
+const mapPersonMethodsToProps = swapiService => {
+  return {
+    getData: swapiService.getAllPeople
+  };
+};
+
+const mapPlanetMethodsToProps = swapiService => {
+  return {
+    getData: swapiService.getAllPlanets
+  };
+};
+
+const mapStarshipMethodsToProps = swapiService => {
+  return {
+    getData: swapiService.getAllStarships
+  };
+};
+
 const PersonList = compose(
   withSwapiService(mapPersonMethodsToProps),
   withData,
